74010b5eb45944e170a511f786b867437d56e909
Ine Maria Nilssen Aanonsen <inema14@uia.no>
2025-09-30 11:30:30 +0200

    test: init db in test to fix CI (no such table: posts)

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,8 +1,18 @@
 from app import create_app
+from app.db import init_db
 
-
-def test_index_returns_ok():
+def test_index_returns_ok(tmp_path):
     app = create_app()
+    app.config.update(
+        DATABASE=str(tmp_path / "test.db"),
+        TESTING=True,
+        SECRET_KEY="test",
+    )
+
+    # For å få til testen på GitHub legges dette til for å initiere testen
+    with app.app_context():
+        init_db()
+
     client = app.test_client()
     resp = client.get("/", follow_redirects=True)
     assert resp.status_code == 200
